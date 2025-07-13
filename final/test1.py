6659462bf11e55b3f1a49f584fc9135fd846f518
abhishek1335 <mandepudiabhishek50@gmail.com>
2025-07-13 23:22:51 +0530

    Delete final directory

--- final/test1.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-try:
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login("darkplayer1335@gmail.com", "mwzh qyhp nnev tobj")
-    print("SMTP connection successful!")
-    server.quit()
-except Exception as e:
-    print("Error:", e)
