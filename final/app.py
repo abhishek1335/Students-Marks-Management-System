1fd2be962750efb69bb41ebccfe33d570e66cc07
abhishek1335 <mandepudiabhishek50@gmail.com>
2025-07-13 23:35:18 +0530

    Update app.py

--- final/app.py
+++ final/app.py
@@ -805,4 +805,5 @@ with app.app_context():
     initialize_all_dbs()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets this PORT
+    app.run(host='0.0.0.0', port=port, debug=True)
